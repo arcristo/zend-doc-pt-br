81e4ce4b449347937a52b5d15c949a377ed26d29
Adiel Cristo <adielcristo@gmail.com>
2017-12-28 19:55:23 -0300

    #6: Update tutorials repo url

--- .travis/build-tutorials.sh
+++ .travis/build-tutorials.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:arcristo/zend-tutorials-pt-br.git
+git clone https://github.com/arcristo/zend-tutorials-pt-br.git
 cd zend-tutorials-pt-br
 git submodule update --init --recursive
 zend-doc-theme-pt-br/bin/build.sh -t "Tutoriais do Zend Framework"
